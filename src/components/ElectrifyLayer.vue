840d266dd24cb5c21a77dc65cd2262d6723e0fd9
Zer02000 <felzel2000@gmail.com>
2020-01-31 11:41:04 +0100

    axios api zugriff

--- src/components/ElectrifyLayer.vue
+++ src/components/ElectrifyLayer.vue
@@ -5,9 +5,33 @@
 </template>
 
 <script>
+    import axios from "axios";
+
     export default {
-        name: "ElectrifyLayer"
+        name: "ElectrifyLayer",
+        components: {
+            axios
+        },
+        stations: null,
+        async mounted() {
+            await this.loadStations();
+            await this.loadStationDetails();
+        },
+        methods: {
+            async loadStations() {
+                let response = await  axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+                this.stations = response;
+            },
+            async loadStationDetails() {
+
+            }
+        }
+
+
     }
+
+
+
 </script>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './plugins/axios'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
