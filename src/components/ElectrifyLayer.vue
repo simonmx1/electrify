7fd58bc8be2461748a61cf208a19620572098d9e
simonmx1 <simon@muscatello.bz.it>
2020-01-31 12:14:46 +0100

    L

--- src/components/ElectrifyLayer.vue
+++ src/components/ElectrifyLayer.vue
@@ -6,13 +6,9 @@
 
 <script>
     import axios from "axios";
-    import L from "leaflet";
 
     export default {
         name: "ElectrifyLayer",
-        components: {
-            L
-        },
         stations: null,
         async mounted() {
             await this.loadStations();
@@ -23,7 +19,6 @@
                 let response = await axios.get('https://ipchannels.integreen-life.bz.it/emobility/rest/get-station-details/');
                 this.stations = response.data;
                 //alert(this.stations);
-                this.L.marker([this.stations.data[1].longitude, this.stations.data[1].latitude]).addTo(this.L.map('el'))
 
             },
             async loadStationDetails() {
