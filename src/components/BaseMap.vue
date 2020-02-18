602b0bf2394fcf906a60e5b053e86861962be2ec
simonmx1 <simon@muscatello.bz.it>
2020-02-18 13:36:04 +0100

    marker try

--- src/components/BaseMap.vue
+++ src/components/BaseMap.vue
@@ -13,12 +13,15 @@
       :url="'http://{s}.tile.osm.org/{z}/{x}/{y}.png'"
       :attribution="attribution"
     />
-    <l-marker></l-marker>
+    <l-marker v-for="l in stations" :key="l.name" :lat-lng="convertLatLng(l)" :icon="icon"/>
   </l-map>
 </template>
 
 <script>
-  import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+  import {LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+  import { latLng, icon, Icon } from 'leaflet';
+  import iconUrl from 'leaflet/dist/images/marker-icon.png';
+  import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
 
   export default {
     name: "GStatBaseMap",
@@ -31,20 +34,18 @@
       height: {type: String, required: false, default: "900px"},
       zoom: {type: Number, required: false, default: 6},
       defaultCenter: {type: Array, required: false},
-      searchTerm: {type: String, required: false, default: null}
+      searchTerm: {type: String, required: false, default: null},
+      stations: {type: Array, required: true}
     },
     methods: {
+      convertLatLng(l) {
+        return latLng(l.latitude, l.longitude);
+      },
       onZoom(event) {
         this.$emit('zoom', event)
       },
       onMove(event) {
         this.$emit('move', event)
-      },
-      onMouseEnter(event) {
-        this.$emit('mouseEnter', event)
-      },
-      onMouseLeave(event) {
-        this.$emit('mouseLeave', event)
       },
       onMouseClick(event) {
         this.$emit('mouseClick', event)
@@ -52,9 +53,14 @@
 
     },
     data() {
+      let customicon = icon(Object.assign({},
+        Icon.Default.prototype.options,
+        {iconUrl, shadowUrl}
+      ));
       return {
+        icon: customicon,
         attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        center: this.defaultCenter
+        center: this.defaultCenter,
       }
     }
   }
