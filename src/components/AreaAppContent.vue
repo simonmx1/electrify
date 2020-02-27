c1d500fb3c4fc81bf215a263e64b45a49685af4b
simonmx1 <simon@muscatello.bz.it>
2020-02-27 11:15:56 +0100

    Marker super nice lol

--- src/components/AreaAppContent.vue
+++ src/components/AreaAppContent.vue
@@ -1,124 +1,123 @@
 <template>
-    <v-content style="height: 100%">
-        <v-container
-                fluid
-                style="height: 100%; padding: 0"
-        >
-            <g-stat-base-map
-                    v-if=this.filteredStations
-                    :zoom="9"
-                    :default-center="[46.4, 11.5]"
-                    :search-term=this.searchTerm
-                    :stations=this.filteredStations
-                    @mouseClick="onMouseClick"
-                    class="float-none"
-                    ref="map"
-                    style="z-index: 0"
-                    @drawer="drawer"
-            />
-            <!--v-dialog
-                    v-model="detailDialog"
-                    app
-                    right
-                    clipped
-                    dark
-                    hide-overlay
-                    max-width="800px"
-            >
-            </v-dialog-->
-        </v-container>
-    </v-content>
+  <v-content style="height: 100%">
+    <v-container
+            fluid
+            style="height: 100%; padding: 0"
+    >
+      <g-stat-base-map
+              v-if=this.filteredStations
+              :zoom="9"
+              :default-center="[46.4, 11.5]"
+              :search-term=this.searchTerm
+              :stations=this.filteredStations
+              @mouseClick="onMouseClick"
+              class="float-none"
+              ref="map"
+              style="z-index: 0"
+              @drawer="drawer"
+      />
+      <!--v-dialog
+              v-model="detailDialog"
+              app
+              right
+              clipped
+              dark
+              hide-overlay
+              max-width="800px"
+      >
+      </v-dialog-->
+    </v-container>
+  </v-content>
 </template>
 
 <script>
-    import GStatBaseMap from "./BaseMap";
-    import axios from "axios";
+  import GStatBaseMap from "./BaseMap";
+  import axios from "axios";
 
+  export default {
+    name: "AreaAppContent",
+    components: {
+      GStatBaseMap
+    },
+    props: {
+      searchTerm: {type: String, required: false, default: null}
+    },
+    watch: {
+      searchTerm: function () {
+        this.filterStations();
+      }
+    },
+    data() {
+      return {
+        stations: null,
+        plugs: null,
+        filteredStations: null
+      }
+    },
+    created() {
+    },
+    async mounted() {
+      await this.loadStations();
+      await this.loadPlugs();
+      this.filterStations();
+    },
+    methods: {
+      drawer(e) {
+        //console.log(this.plugs);
+        let event = [e, this.getPlugsFromStation(e)];
 
-    export default {
-        name: "AreaAppContent",
-        components: {
-            GStatBaseMap
-        },
-        props: {
-            searchTerm: {type: String, required: false, default: null}
-        },
-        watch: {
-            searchTerm: function () {
-                this.filterStations();
-            }
-        },
-        data() {
-            return {
-                stations: null,
-                plugs: null,
-                filteredStations: null
-            }
-        },
-        created() {
-        },
-        async mounted() {
-            await this.loadStations();
-            await this.loadPlugs();
-            this.filterStations();
-        },
-        methods: {
-            drawer(e) {
-                //console.log(this.plugs);
-                let event = [e, this.getPlugsFromStation(e)];
-
-                this.$emit("drawer", event);
-            },
-            async loadStations() {
-                let response = await axios.get('https://ipchannels.integreen-life.bz.it/emobility/rest/get-station-details/');
-                this.stations = response.data;
-                //alert(this.stations);
-            },
-            async loadPlugs() {
-                let response = await axios.get('https://ipchannels.integreen-life.bz.it/emobility/rest/plugs/get-station-details/');
-                this.plugs = response.data;
-                //alert(this.plugs[0].latitude);
-            },
-            getPlugsFromStation(id) {
-                let ret = [];
-                for (let i = 0; i < this.plugs.length; i++) {
-                    let plug = this.plugs[i];
-                    if (plug.parentStation == id) {
-                        ret.push(plug);
-                    }
-                }
-                return ret;
-            },
-            filterStations() {
-                this.filteredStations = new Array();
-                for (let i = 0; i < this.stations.length; i++) {
-                    if(this.stations[i].accessType == "PUBLIC") {
-                        console.log("public");
-                        if (this.searchTerm == null) {
-                            this.filteredStations.push(this.stations[i]);
-                        } else {
-                            if (this.stations[i].name.contains(this.searchTerm)) {
-                                this.filteredStations.push(this.stations[i]);
-                            }
-                        }
-                    }
-                }
-
-            }
+        this.$emit("drawer", event);
+      },
+      async loadStations() {
+        let response = await axios.get('https://ipchannels.integreen-life.bz.it/emobility/rest/get-station-details/');
+        this.stations = response.data;
+        //alert(this.stations);
+      },
+      async loadPlugs() {
+        let response = await axios.get('https://ipchannels.integreen-life.bz.it/emobility/rest/plugs/get-station-details/');
+        this.plugs = response.data;
+        //alert(this.plugs[0].latitude);
+      },
+      getPlugsFromStation(id) {
+        let ret = [];
+        for (let i = 0; i < this.plugs.length; i++) {
+          let plug = this.plugs[i];
+          if (plug.parentStation == id) {
+            ret.push(plug);
+          }
         }
+        return ret;
+      },
+      filterStations() {
+        this.filteredStations = [];
+        for (let i = 0; i < this.stations.length; i++) {
+          if (this.stations[i].accessType == "PUBLIC") {
+            console.log("public");
+            if (this.searchTerm == null) {
+              this.filteredStations.push(this.stations[i]);
+            } else {
+              if (this.stations[i].name.contains(this.searchTerm)) {
+                this.filteredStations.push(this.stations[i]);
+              }
+            }
+          }
+        }
+
+      }
     }
+  }
 </script>
 
 <style>
-    table {
-        width: 100%;
-    }
+  table {
+    width: 100%;
+  }
 
-    tr {
-        width: 100%;
-    }
+  tr {
+    width: 100%;
+  }
 
-    .zindexmax {
-        z-index: 1000;
-    }
+  .zindexmax {
+    z-index: 1000;
+  }
 </style>
--- src/components/BaseMap.vue
+++ src/components/BaseMap.vue
@@ -1,72 +1,135 @@
 <template>
-    <l-map
-            :style="{
+  <l-map
+          :style="{
       height: '100%'
     }"
-            :zoom="zoom"
-            :center="center"
-            @update:zoom="onZoom"
-            @move="onMove"
-            ref="map"
-    >
-        <l-tile-layer
-                :url="'http://{s}.tile.osm.org/{z}/{x}/{y}.png'"
-                :attribution="attribution"
-        />
-        <l-marker
-                v-for="l in stations"
-                :key="l.name"
-                :lat-lng="convertLatLng(l)"
-                v-on:click="click(l)"
-        ></l-marker>
-    </l-map>
+          :zoom="zoom"
+          :center="center"
+          @update:zoom="onZoom"
+          @move="onMove"
+          ref="map"
+  >
+    <l-tile-layer
+            :url="'http://{s}.tile.osm.org/{z}/{x}/{y}.png'"
+            :attribution="attribution"
+    />
+    <l-marker
+            v-for="l in stations"
+            :key="l.name"
+            :lat-lng="convertLatLng(l)"
+            v-on:click="click(l)"
+            :icon="getMarkerIcon(l)"
+    ></l-marker>
+  </l-map>
 </template>
 
 <script>
-    import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
-    import {latLng} from 'leaflet';
+  import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+  import L, {latLng} from 'leaflet';
 
-    export default {
-        name: "GStatBaseMap",
-        components: {
-            LMap,
-            LTileLayer,
-            LMarker
-        },
-        props: {
-            height: {type: String, required: false, default: "900px"},
-            zoom: {type: Number, required: false, default: 6},
-            defaultCenter: {type: Array, required: false},
-            searchTerm: {type: String, required: false, default: null},
-            stations: {type: Array, required: true}
-        },
-        methods: {
-            convertLatLng(l) {
-                return latLng(l.latitude, l.longitude);
-            },
-            onZoom(event) {
-                this.$emit('zoom', event)
-            },
-            onMove(event) {
-                this.$emit('move', event)
-            },
-            onMouseClick(event) {
-                this.$emit('mouseClick', event)
-            },
-            click(event) {
-                this.$emit('drawer', event);
-            }
+  export default {
+    name: "GStatBaseMap",
+    components: {
+      LMap,
+      LTileLayer,
+      LMarker
+    },
+    props: {
+      height: {type: String, required: false, default: "900px"},
+      zoom: {type: Number, required: false, default: 6},
+      defaultCenter: {type: Array, required: false},
+      searchTerm: {type: String, required: false, default: null},
+      stations: {type: Array, required: true},
+      //Styling Marker
+      iconFunc: {type: Function, required: false, default: defaultIconFunc},
+      iconColorFunc: {type: Function, required: false, default: defaultIconColorFunc},
+      markerColorFunc: {type: Function, required: false, default: defaultMarkerColorFunc},
 
-        },
-        data() {
-            return {
-                attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-                center: this.defaultCenter,
-            }
-        }
+
+    },
+    methods: {
+      convertLatLng(l) {
+        return latLng(l.latitude, l.longitude);
+      },
+      onZoom(event) {
+        this.$emit('zoom', event)
+      },
+      onMove(event) {
+        this.$emit('move', event)
+      },
+      onMouseClick(event) {
+        this.$emit('mouseClick', event)
+      },
+      click(event) {
+        this.$emit('drawer', event);
+      },
+      getMarkerIcon: function (item) {
+        return new this.IconMaterial({
+          icon: this.iconFunc(item),
+          iconColor: this.iconColorFunc(item),
+          markerColor: this.markerColorFunc(item)
+        })
+      }
+    },
+    data() {
+      return {
+        IconMaterial: L.Icon.extend({
+          options: {
+            className: 'l-icon-material',
+            icon: null,
+            markerColor: '#FFFFFF',
+            iconColor: '#000000',
+            outlineColor: '#000000',
+            outlineWidth: '1'
+          },
+          initialize: function (options) {
+            this.options = L.Util.setOptions(this, options);
+          },
+          createIcon: function () {
+            let options = L.Util.setOptions(this, this.options);
+            let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+            let path = document.createElementNS('http://www.w3.org/2000/svg', "path");
+            let icon = document.createElementNS('http://www.w3.org/2000/svg', "foreignObject");
+
+            svg.setAttribute('width', '31');
+            svg.setAttribute('height', '42');
+            svg.setAttribute('class', 'l-icon-material');
+            svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+
+            path.setAttributeNS(null, "d", "M15.6,1c-7.7,0-14,6.3-14,14c0,10.5,14,26,14,26s14-15.5,14-26C29.6,7.3,23.3,1,15.6,1z");
+            path.setAttribute('fill', options.markerColor);
+            path.setAttribute('stroke', options.outlineColor);
+            path.setAttribute('stroke-width', options.outlineWidth);
+            svg.append(path);
+
+            icon.setAttribute('width', '31');
+            icon.setAttribute('height', '42');
+            icon.setAttribute('class', `node mdi ${options.icon}`);
+            icon.setAttribute('fill', options.iconColor);
+            svg.appendChild(icon);
+
+            return svg;
+          }
+        }),
+        attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        center: this.defaultCenter
+      }
     }
+  }
+
+  function defaultIconFunc() {
+    return null;
+  }
+
+  function defaultMarkerColorFunc() {
+    return "#FFFFFF"
+  }
+
+  function defaultIconColorFunc() {
+    return "#000000"
+  }
 </script>
 
 <style scoped>
-    @import "../../node_modules/leaflet/dist/leaflet.css";
+  @import "../../node_modules/leaflet/dist/leaflet.css";
 </style>
