1b157641b0e534cf3a22022a5d7d670535316647
zer02000 <felzel2000@gmail.com>
2020-02-29 16:52:47 +0100

    sel

--- src/App.vue
+++ src/App.vue
@@ -1,109 +1,109 @@
 <template>
-  <v-app>
-    <v-app-bar
-            app
-            color=primary
-            dark
-            clipped-right
-            clipped-left
-    >
-      <v-toolbar-title>
-        <v-toolbar-title>
-          <v-icon x-large>mdi-battery-charging-100</v-icon>
-          ELECTRIFY
-        </v-toolbar-title>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-
-      <v-switch
-              v-model="publiccheck"
-              :label="`Public`"
-
-      ></v-switch>
-
-      <v-text-field
-              v-model="searchTerm"
-              :append-icon-cb="() => {}"
-              placeholder="Nach Ladestation suchen..."
-              single-line
-              append-icon="search"
-              color="white"
-              hide-details
-      />
-
-    </v-app-bar>
-    <v-navigation-drawer
-            v-model="menu"
-            app
-            clipped
-            dark
-            mini-variant
-            class="white--text">
-    </v-navigation-drawer>
-    <v-navigation-drawer
-            v-model="drawer"
-            app
-            clipped
-            dark
-            right
-            class="white--text"
-            v-if="station != null"
-    >
-
-      <v-icon x-large @click.stop="station = null">mdi-chevron-double-right</v-icon>
-      <br>
-      name: {{this.station.name}}
-      <ul>
-        <v-list v-for="plug in this.plugs" :key="plug.id">
-          <li>
-            {{plug.name}}
+    <v-app>
+        <v-app-bar
+                app
+                color=primary
+                dark
+                clipped-right
+                clipped-left
+        >
+            <v-toolbar-title>
+                <v-toolbar-title>
+                    <v-icon x-large>mdi-battery-charging-100</v-icon>
+                    ELECTRIFY
+                </v-toolbar-title>
+            </v-toolbar-title>
             <v-spacer></v-spacer>
-          </li>
-        </v-list>
-      </ul>
 
-    </v-navigation-drawer>
-    <app-content :search-term=searchTerm
-                      :publiccheck=publiccheck
-                      @drawer="draw">
+            <v-switch
+                    v-model="publiccheck"
+                    :label="`Public`"
 
-    </app-content>
-    <v-footer
-            app
-            color=primary
-            dark
-    >Electrify
-      <v-spacer/>
-      © 2020
-    </v-footer>
-  </v-app>
+            ></v-switch>
+
+            <v-text-field
+                    v-model="searchTerm"
+                    :append-icon-cb="() => {}"
+                    placeholder="Nach Ladestation suchen..."
+                    single-line
+                    append-icon="search"
+                    color="white"
+                    hide-details
+            />
+
+        </v-app-bar>
+        <v-navigation-drawer
+                v-model="menu"
+                app
+                clipped
+                dark
+                mini-variant
+                class="white--text">
+        </v-navigation-drawer>
+        <v-navigation-drawer
+                v-model="drawer"
+                app
+                clipped
+                dark
+                right
+                class="white--text"
+                v-if="station != null"
+        >
+
+            <v-icon x-large @click.stop="station = null">mdi-chevron-double-right</v-icon>
+            <br>
+            name: {{this.station.name}}
+            <ul>
+                <v-list v-for="plug in this.plugs" :key="plug.id">
+                    <li>
+                        {{plug.name}}
+                        <v-spacer></v-spacer>
+                    </li>
+                </v-list>
+            </ul>
+
+        </v-navigation-drawer>
+        <app-content :search-term=searchTerm
+                     :publiccheck=publiccheck
+                     @drawer="draw">
+
+        </app-content>
+        <v-footer
+                app
+                color=primary
+                dark
+        >Electrify
+            <v-spacer/>
+            © 2020
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-  import AppContent from './components/AppContent';
+    import AppContent from './components/AppContent';
 
-  export default {
-    name: 'App',
+    export default {
+        name: 'App',
 
-    components: {
-      AppContent
-    },
+        components: {
+            AppContent
+        },
 
-    data: () => ({
-      searchTerm: null,
-      publiccheck: true,
-      drawer: true,
-      menu: null,
-      station: null,
-      plugs: null
-    }),
-    methods: {
-      draw(event) {
-        this.station = event[0];
-        this.plugs = event[1];
-        //console.log(this.station);
-        //console.log(this.plugs);
-      }
-    }
-  };
+        data: () => ({
+            searchTerm: null,
+            publiccheck: true,
+            drawer: true,
+            menu: null,
+            station: null,
+            plugs: null
+        }),
+        methods: {
+            draw(event) {
+                this.station = event[0];
+                this.plugs = event[1];
+                //console.log(this.station);
+                //console.log(this.plugs);
+            }
+        }
+    };
 </script>
--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,130 +1,132 @@
 <template>
-  <v-content style="height: 100%">
-    <v-container
-            fluid
-            style="height: 100%; padding: 0"
-    >
-      <base-map
-              v-if=this.filteredStations
-              :zoom="9"
-              :default-center="[46.4, 11.5]"
-              :search-term=this.searchTerm
-              :stations=this.filteredStations
-              @mouseClick="onMouseClick"
-              class="float-none"
-              ref="map"
-              style="z-index: 0"
-              @drawer="drawer"
-      />
-      <!--v-dialog
-              v-model="detailDialog"
-              app
-              right
-              clipped
-              dark
-              hide-overlay
-              max-width="800px"
-      >
-      </v-dialog-->
-    </v-container>
-  </v-content>
+    <v-content style="height: 100%">
+        <v-container
+                fluid
+                style="height: 100%; padding: 0"
+        >
+            <base-map
+                    v-if=this.filteredStations
+                    :zoom="9"
+                    :default-center="[46.4, 11.5]"
+                    :search-term=this.searchTerm
+                    :stations=this.filteredStations
+                    @mouseClick="onMouseClick"
+                    class="float-none"
+                    ref="map"
+                    style="z-index: 0"
+                    @drawer="drawer"
+            />
+            <!--v-dialog
+                    v-model="detailDialog"
+                    app
+                    right
+                    clipped
+                    dark
+                    hide-overlay
+                    max-width="800px"
+            >
+            </v-dialog-->
+        </v-container>
+    </v-content>
 </template>
 
 <script>
-  import BaseMap from "./BaseMap";
-  import axios from "axios";
+    import BaseMap from "./BaseMap";
+    import axios from "axios";
 
 
-  export default {
-    name: "AppContent",
-    components: {
-      BaseMap
-    },
-    props: {
-      searchTerm: {type: String, required: false, default: null},
-      publiccheck: {type: Boolean, required: false, default: true}
-    },
-    watch: {
-      searchTerm: function () {
-        this.filterStations();
-      },
-      publiccheck: function () {
-        console.log("public change");
-        this.filterStations();
-      }
-    },
-    data() {
-      return {
-        stations: null,
-        plugs: null,
-        filteredStations: null,
-      }
-    },
-    created() {
-    },
-    async mounted() {
-      await this.loadStations();
-      await this.loadPlugs();
-      this.filterStations();
-    },
-    methods: {
-      drawer(e) {
-        //console.log(this.plugs);
-        let event = [e, this.getPlugsFromStation(e)];
-
-        this.$emit("drawer", event);
-      },
-      async loadStations() {
-        let response = await axios.get('https://ipchannels.integreen-life.bz.it/emobility/rest/get-station-details/');
-        this.stations = response.data;
-        //alert(this.stations);
-      },
-      async loadPlugs() {
-        let response = await axios.get('https://ipchannels.integreen-life.bz.it/emobility/rest/plugs/get-station-details/');
-        this.plugs = response.data;
-        //alert(this.plugs[0].latitude);
-      },
-      getPlugsFromStation(id) {
-        let ret = [];
-        for (let i = 0; i < this.plugs.length; i++) {
-          let plug = this.plugs[i];
-          if (plug.parentStation == id) {
-            ret.push(plug);
-          }
-        }
-        return ret;
-      },
-      async filterStations() {
-        this.filteredStations = [];
-        console.log("filter" + this.publiccheck);
-        for (let i = 0; i < this.stations.length; i++) {
-          if (!this.publiccheck || this.stations[i].accessType == "PUBLIC") {
-            console.log("public");
-            if (this.searchTerm == null) {
-              this.filteredStations.push(this.stations[i]);
-            } else {
-              if (this.stations[i].name.toLowerCase().includes(this.searchTerm.toLowerCase())) {
-                this.filteredStations.push(this.stations[i]);
-              }
+    export default {
+        name: "AppContent",
+        components: {
+            BaseMap
+        },
+        props: {
+            searchTerm: {type: String, required: false, default: null},
+            publiccheck: {type: Boolean, required: false, default: true}
+        },
+        watch: {
+            searchTerm: function () {
+                console.log("search");
+                this.filterStations();
+            },
+            publiccheck: function () {
+                console.log("public change");
+                this.filterStations();
             }
-          }
+        },
+        data() {
+            return {
+                stations: null,
+                plugs: null,
+                filteredStations: null,
+            }
+        },
+        created() {
+        },
+        async mounted() {
+            await this.loadStations();
+            await this.loadPlugs();
+            this.filterStations();
+        },
+        methods: {
+            drawer(e) {
+                //console.log(this.plugs);
+                let event = [e, this.getPlugsFromStation(e)];
+
+                this.$emit("drawer", event);
+            },
+            async loadStations() {
+                let response = await axios.get('https://ipchannels.integreen-life.bz.it/emobility/rest/get-station-details/');
+                this.stations = response.data;
+                //alert(this.stations);
+            },
+            async loadPlugs() {
+                let response = await axios.get('https://ipchannels.integreen-life.bz.it/emobility/rest/plugs/get-station-details/');
+                this.plugs = response.data;
+                //alert(this.plugs[0].latitude);
+            },
+            getPlugsFromStation(id) {
+                let ret = [];
+                for (let i = 0; i < this.plugs.length; i++) {
+                    let plug = this.plugs[i];
+                    if (plug.parentStation == id) {
+                        ret.push(plug);
+                    }
+                }
+                return ret;
+            },
+            async filterStations() {
+                let filteredStations1 = [];
+                console.log("filter" + this.publiccheck);
+                for (let i = 0; i < this.stations.length; i++) {
+                    if (!this.publiccheck || this.stations[i].accessType == "PUBLIC") {
+                        //console.log("public");
+                        if (this.searchTerm == null) {
+                            filteredStations1.push(this.stations[i]);
+                        } else {
+                            if (this.stations[i].name.toLowerCase().includes(this.searchTerm.toLowerCase())) {
+                                filteredStations1.push(this.stations[i]);
+                            }
+                        }
+                    }
+                }
+                this.filteredStations = filteredStations1;
+                console.log("finished");
+            }
         }
-        console.log("finished");
-      }
     }
-  }
 </script>
 
 <style>
-  table {
-    width: 100%;
-  }
+    table {
+        width: 100%;
+    }
 
-  tr {
-    width: 100%;
-  }
+    tr {
+        width: 100%;
+    }
 
-  .zindexmax {
-    z-index: 1000;
-  }
+    .zindexmax {
+        z-index: 1000;
+    }
 </style>
