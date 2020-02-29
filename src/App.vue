dc94fbe6b67f682885605cf432dfd86e77cd208c
simonmx1 <simon@muscatello.bz.it>
2020-02-29 17:57:52 +0100

    working

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,106 @@
 <template>
-  <v-app>
-    <v-app-bar
-            app
-            color=primary
-            dark
-            clipped-right
-            clipped-left
-    >
-      <v-toolbar-title>
-        <v-icon x-large>mdi-battery-charging-100</v-icon>
-        ELECTRIFY
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-switch
-              v-model="publicCheck"
-              :label="'Public'"
-
-      ></v-switch>
-
-      <v-text-field
-              v-model="searchTerm"
-              :append-icon-cb="() => {}"
-              placeholder="Nach Ladestation suchen..."
-              single-line
-              append-icon="search"
-              color="white"
-              hide-details
-      />
-
-    </v-app-bar>
-    <v-navigation-drawer
-            v-model="menu"
-            app
-            clipped
-            dark
-            mini-variant
-            class="white--text">
-    </v-navigation-drawer>
-    <v-navigation-drawer
-            v-model="drawer"
-            app
-            clipped
-            dark
-            right
-            class="white--text"
-            v-if="station != null"
-    >
-
-      <v-icon x-large @click.stop="station = null">mdi-chevron-double-right</v-icon>
-      <br>
-      name: {{this.station.name}}
-      <ul>
-        <v-list v-for="plug in this.plugs" :key="plug.id">
-          <li>
-            {{plug.name}}
+    <v-app>
+        <v-app-bar
+                app
+                color=primary
+                dark
+                clipped-right
+                clipped-left
+        >
+            <v-toolbar-title>
+                <v-icon x-large>mdi-battery-charging-100</v-icon>
+                ELECTRIFY
+            </v-toolbar-title>
             <v-spacer></v-spacer>
-          </li>
-        </v-list>
-      </ul>
+            <v-switch
+                    v-model="publicCheck"
+                    :label="'Public'"
 
-    </v-navigation-drawer>
-    <app-content :search-term=searchTerm
-                 :public-check=publicCheck
-                 @drawer="draw">
+            ></v-switch>
 
-    </app-content>
-    <v-footer
-            app
-            color=primary
-            dark
-    >Electrify
-      <v-spacer/>
-      © 2020
-    </v-footer>
-  </v-app>
+            <v-text-field
+                    v-model="searchTerm"
+                    :append-icon-cb="() => {}"
+                    placeholder="Nach Ladestation suchen..."
+                    single-line
+                    append-icon="search"
+                    color="white"
+                    hide-details
+            />
+
+        </v-app-bar>
+        <v-navigation-drawer
+                v-model="menu"
+                app
+                clipped
+                dark
+                mini-variant
+                class="white--text">
+        </v-navigation-drawer>
+        <v-navigation-drawer
+                v-model="drawer"
+                app
+                clipped
+                dark
+                right
+                class="white--text"
+                v-if="station != null"
+        >
+
+            <v-icon x-large @click.stop="station = null">mdi-chevron-double-right</v-icon>
+            <br>
+            name: {{this.station.name}}
+            <ul>
+                <v-list v-for="plug in this.plugs" :key="plug.id">
+                    <li>
+                        {{plug.name}}
+                        <v-spacer></v-spacer>
+                    </li>
+                </v-list>
+            </ul>
+
+        </v-navigation-drawer>
+        <app-content :search-term=searchTerm
+                     :public-check=publicCheck
+                     @drawer="draw">
+
+        </app-content>
+        <v-footer
+                app
+                color=primary
+                dark
+        >Electrify
+            <v-spacer/>
+            © 2020
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-  import AppContent from './components/AppContent';
+    import AppContent from './components/AppContent';
 
-  export default {
-    name: 'App',
+    export default {
+        name: 'App',
 
-    components: {
-      AppContent
-    },
+        components: {
+            AppContent
+        },
 
-    data: () => ({
-      searchTerm: null,
-      publicCheck: true,
-      drawer: false,
-      menu: null,
-      station: null,
-      plugs: null
-    }),
-    methods: {
-      draw(event) {
-        this.station = event[0];
-        this.plugs = event[1];
-        //console.log(this.station);
-        //console.log(this.plugs);
-      }
-    }
-  };
+        data: () => ({
+            searchTerm: null,
+            publicCheck: true,
+            drawer: false,
+            menu: null,
+            station: null,
+            plugs: null
+        }),
+        methods: {
+            draw(event) {
+                this.station = event[0];
+                this.plugs = event[1];
+                //console.log(this.station);
+                //console.log(this.plugs);
+            }
+        }
+    };
 </script>
--- src/components/BaseMap.vue
+++ src/components/BaseMap.vue
@@ -1,178 +1,179 @@
 <template>
-  <l-map
-          :style="{
+    <l-map
+            :style="{
       height: '100%'
     }"
-          :zoom="zoom"
-          :center="center"
-          @update:zoom="onZoom"
-          @move="onMove"
-          ref="map"
-  >
-    <l-tile-layer
-            :url="'http://{s}.tile.osm.org/{z}/{x}/{y}.png'"
-            :attribution="attribution"
-    />
-    <marker-cluster
-            v-if="filteredStations"
+            :zoom="zoom"
+            :center="center"
+            @update:zoom="onZoom"
+            @move="onMove"
+            ref="map"
     >
-      <l-marker
-              v-for="l in filteredStations"
-              :key="l.name"
-              :lat-lng="convertLatLng(l)"
-              v-on:click="click(l)"
-              :icon="getMarkerIcon(l)"
-      ></l-marker>
-    </marker-cluster>
-  </l-map>
+        <l-tile-layer
+                :url="'http://{s}.tile.osm.org/{z}/{x}/{y}.png'"
+                :attribution="attribution"
+        />
+        <marker-cluster
+                v-if="filteredStations"
+        >
+            <l-marker
+                    v-for="l in filteredStations"
+                    :key="l.id"
+                    :lat-lng="convertLatLng(l)"
+                    v-on:click="click(l)"
+                    :icon="getMarkerIcon(l)"
+            ></l-marker>
+        </marker-cluster>
+    </l-map>
 </template>
 
 <script>
-  import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
-  import L, {latLng} from 'leaflet';
-  import Vue2LeafletMarkercluster from 'vue2-leaflet-markercluster';
+    import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+    import L, {latLng} from 'leaflet';
+    import Vue2LeafletMarkercluster from 'vue2-leaflet-markercluster';
 
-  export default {
-    name: "BaseMap",
-    components: {
-      LMap,
-      LTileLayer,
-      LMarker,
-      'marker-cluster': Vue2LeafletMarkercluster,
-    },
-    props: {
-      height: {type: String, required: false, default: "900px"},
-      zoom: {type: Number, required: false, default: 6},
-      defaultCenter: {type: Array, required: false},
-      searchTerm: {type: String, required: false, default: null},
-      publicCheck: {type: Boolean, required: false, default: true},
-      stations: {type: Array, required: true},
-      //Styling Marker
-      iconFunc: {type: Function, required: false, default: defaultIconFunc},
-      iconColorFunc: {type: Function, required: false, default: defaultIconColorFunc},
-      markerColorFunc: {type: Function, required: false, default: defaultMarkerColorFunc},
-    },
-    watch: {
-      searchTerm: function () {
-        this.filterStations();
-      },
-      publicCheck: function () {
-        this.filterStations();
-      }
-    },
-    created() {
-      this.filteredStations = this.stations;
-    },
-    methods: {
-      convertLatLng(l) {
-        return latLng(l.latitude, l.longitude);
-      },
-      onZoom(event) {
-        this.$emit('zoom', event)
-      },
-      onMove(event) {
-        this.$emit('move', event)
-      },
-      click(event) {
-        this.$emit('drawer', event);
-      },
-      getMarkerIcon: function (item) {
-        return new this.IconMaterial({
-          icon: this.iconFunc(item),
-          iconColor: this.iconColorFunc(item),
-          markerColor: this.markerColorFunc(item)
-        })
-      },
-      async filterStations() {
-        let filteredStations = [];
-        for (let i = 0; i < this.stations.length; i++) {
-          if (!this.publicCheck || this.stations[i].accessType == "PUBLIC") {
-            if (this.searchTerm == null) {
-              filteredStations.push(this.stations[i]);
-            } else {
-              if (this.stations[i].name.toLowerCase().includes(this.searchTerm.toLowerCase())) {
-                filteredStations.push(this.stations[i]);
-              }
+    export default {
+        name: "BaseMap",
+        components: {
+            LMap,
+            LTileLayer,
+            LMarker,
+            'marker-cluster': Vue2LeafletMarkercluster,
+        },
+        props: {
+            height: {type: String, required: false, default: "900px"},
+            zoom: {type: Number, required: false, default: 6},
+            defaultCenter: {type: Array, required: false},
+            searchTerm: {type: String, required: false, default: null},
+            publicCheck: {type: Boolean, required: false, default: true},
+            stations: {type: Array, required: true},
+            //Styling Marker
+            iconFunc: {type: Function, required: false, default: defaultIconFunc},
+            iconColorFunc: {type: Function, required: false, default: defaultIconColorFunc},
+            markerColorFunc: {type: Function, required: false, default: defaultMarkerColorFunc},
+        },
+        watch: {
+            searchTerm: function () {
+                this.filterStations();
+            },
+            publicCheck: function () {
+                this.filterStations();
             }
-          }
+        },
+        mounted() {
+            this.filterStations();
+        },
+        methods: {
+            convertLatLng(l) {
+                return latLng(l.latitude, l.longitude);
+            },
+            onZoom(event) {
+                this.$emit('zoom', event)
+            },
+            onMove(event) {
+                this.$emit('move', event)
+            },
+            click(event) {
+                this.$emit('drawer', event);
+            },
+            getMarkerIcon: function (item) {
+                return new this.IconMaterial({
+                    icon: this.iconFunc(item),
+                    iconColor: this.iconColorFunc(item),
+                    markerColor: this.markerColorFunc(item)
+                })
+            },
+            async filterStations() {
+                this.filteredStations = null;
+                let filteredStations = [];
+                for (let i = 0; i < this.stations.length; i++) {
+                    if (this.stations[i] && !this.publicCheck || this.stations[i].accessType == "PUBLIC") {
+                        if (this.searchTerm == null) {
+                            filteredStations.push(this.stations[i]);
+                        } else {
+                            if (this.stations[i].city.toLowerCase().includes(this.searchTerm.toLowerCase())) {
+                                filteredStations.push(this.stations[i]);
+                            }
+                        }
+                    }
+                }
+                this.filteredStations = filteredStations;
+                console.log("finished");
+            }
+        },
+        data() {
+            return {
+                filteredStations: null,
+                attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+                center: this.defaultCenter,
+                IconMaterial: L.Icon.extend({
+                    options: {
+                        className: 'l-icon-material',
+                        icon: null,
+                        markerColor: '#FFFFFF',
+                        iconColor: '#000000',
+                        outlineColor: '#000000',
+                        outlineWidth: '1'
+                    },
+                    initialize: function (options) {
+                        this.options = L.Util.setOptions(this, options);
+                    },
+                    createIcon: function () {
+                        let options = L.Util.setOptions(this, this.options);
+                        let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                        let path = document.createElementNS('http://www.w3.org/2000/svg', "path");
+                        let icon = document.createElementNS('http://www.w3.org/2000/svg', "foreignObject");
+
+                        svg.setAttribute('width', '31');
+                        svg.setAttribute('height', '42');
+                        svg.setAttribute('class', 'l-icon-material');
+                        svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+
+                        path.setAttributeNS(null, "d", "M15.6,1c-7.7,0-14,6.3-14,14c0,10.5,14,26,14,26s14-15.5,14-26C29.6,7.3,23.3,1,15.6,1z");
+                        path.setAttribute('fill', options.markerColor);
+                        path.setAttribute('stroke', options.outlineColor);
+                        path.setAttribute('stroke-width', options.outlineWidth);
+                        svg.append(path);
+
+                        icon.setAttribute('width', '31');
+                        icon.setAttribute('height', '42');
+                        icon.setAttribute('class', `node mdi ${options.icon}`);
+                        icon.setAttribute('fill', options.iconColor);
+                        svg.appendChild(icon);
+
+                        return svg;
+                    }
+                })
+            }
         }
-        this.filteredStations = filteredStations;
-        console.log("finished");
-      }
-    },
-    data() {
-      return {
-        filteredStations: null,
-        attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        center: this.defaultCenter,
-        IconMaterial: L.Icon.extend({
-          options: {
-            className: 'l-icon-material',
-            icon: null,
-            markerColor: '#FFFFFF',
-            iconColor: '#000000',
-            outlineColor: '#000000',
-            outlineWidth: '1'
-          },
-          initialize: function (options) {
-            this.options = L.Util.setOptions(this, options);
-          },
-          createIcon: function () {
-            let options = L.Util.setOptions(this, this.options);
-            let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-            let path = document.createElementNS('http://www.w3.org/2000/svg', "path");
-            let icon = document.createElementNS('http://www.w3.org/2000/svg', "foreignObject");
-
-            svg.setAttribute('width', '31');
-            svg.setAttribute('height', '42');
-            svg.setAttribute('class', 'l-icon-material');
-            svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
-
-            path.setAttributeNS(null, "d", "M15.6,1c-7.7,0-14,6.3-14,14c0,10.5,14,26,14,26s14-15.5,14-26C29.6,7.3,23.3,1,15.6,1z");
-            path.setAttribute('fill', options.markerColor);
-            path.setAttribute('stroke', options.outlineColor);
-            path.setAttribute('stroke-width', options.outlineWidth);
-            svg.append(path);
-
-            icon.setAttribute('width', '31');
-            icon.setAttribute('height', '42');
-            icon.setAttribute('class', `node mdi ${options.icon}`);
-            icon.setAttribute('fill', options.iconColor);
-            svg.appendChild(icon);
-
-            return svg;
-          }
-        })
-      }
     }
-  }
 
-  function defaultIconFunc() {
-    return "mdi-battery-charging-100";
-  }
+    function defaultIconFunc() {
+        return "mdi-battery-charging-100";
+    }
 
-  function defaultMarkerColorFunc() {
-    return "#FFFFFF"
-  }
+    function defaultMarkerColorFunc() {
+        return "#FFFFFF"
+    }
 
-  function defaultIconColorFunc() {
-    return "#000000"
-  }
+    function defaultIconColorFunc() {
+        return "#000000"
+    }
 </script>
 
 <style>
-  @import "~leaflet.markercluster/dist/MarkerCluster.css";
-  @import "~leaflet.markercluster/dist/MarkerCluster.Default.css";
-  @import "../../node_modules/leaflet/dist/leaflet.css";
+    @import "~leaflet.markercluster/dist/MarkerCluster.css";
+    @import "~leaflet.markercluster/dist/MarkerCluster.Default.css";
+    @import "../../node_modules/leaflet/dist/leaflet.css";
 
-  .l-icon-material {
-    margin-top: -42px;
-    margin-left: -17px;
-    position: absolute;
-    left: 0;
-    top: 0;
-    display: block;
-    text-align: center;
-    font-size: 20px;
-  }
+    .l-icon-material {
+        margin-top: -42px;
+        margin-left: -17px;
+        position: absolute;
+        left: 0;
+        top: 0;
+        display: block;
+        text-align: center;
+        font-size: 20px;
+    }
 </style>
