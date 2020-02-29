7e1715cbd4525cbd69d48aa397f28a1486d2f957
zer02000 <felzel2000@gmail.com>
2020-02-29 17:57:02 +0100

    sel

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,87 +1,88 @@
 <template>
-  <v-content style="height: 100%">
-    <v-container
-            fluid
-            style="height: 100%; padding: 0"
-    >
-      <base-map
-              :zoom="9"
-              :default-center="[46.4, 11.5]"
-              :search-term=searchTerm
-              :public-check="publicCheck"
-              :stations="stations"
-              class="float-none"
-              ref="map"
-              style="z-index: 0"
-              @drawer="drawer()"
-      />
-    </v-container>
-  </v-content>
+    <v-content style="height: 100%">
+        <v-container
+                fluid
+                style="height: 100%; padding: 0"
+        >
+            <base-map
+                    v-if="stations"
+                    :zoom="9"
+                    :default-center="[46.4, 11.5]"
+                    :search-term=searchTerm
+                    :public-check="publicCheck"
+                    :stations="stations"
+                    class="float-none"
+                    ref="map"
+                    style="z-index: 0"
+                    @drawer="drawer()"
+            />
+        </v-container>
+    </v-content>
 </template>
 
 <script>
-  import BaseMap from "./BaseMap";
-  import axios from "axios";
+    import BaseMap from "./BaseMap";
+    import axios from "axios";
 
-  export default {
-    name: "AppContent",
-    components: {
-      BaseMap
-    },
-    props: {
-      searchTerm: {type: String, required: false, default: null},
-      publicCheck: {type: Boolean, required: false, default: true}
-    },
-    data() {
-      return {
-        stations: null,
-        plugs: null,
-      }
-    },
-    created() {
-    },
-    async mounted() {
-      await this.loadStations();
-      await this.loadPlugs();
-    },
-    methods: {
-      drawer(e) {
-        let event = [e, this.getPlugsFromStation(e)];
+    export default {
+        name: "AppContent",
+        components: {
+            BaseMap
+        },
+        props: {
+            searchTerm: {type: String, required: false, default: null},
+            publicCheck: {type: Boolean, required: false, default: true}
+        },
+        data() {
+            return {
+                stations: null,
+                plugs: null,
+            }
+        },
+        created() {
+        },
+        async mounted() {
+            await this.loadStations();
+            await this.loadPlugs();
+        },
+        methods: {
+            drawer(e) {
+                let event = [e, this.getPlugsFromStation(e)];
 
-        this.$emit("drawer", event);
-      },
-      async loadStations() {
-        let response = await axios.get('https://ipchannels.integreen-life.bz.it/emobility/rest/get-station-details/');
-        this.stations = response.data;
-      },
-      async loadPlugs() {
-        let response = await axios.get('https://ipchannels.integreen-life.bz.it/emobility/rest/plugs/get-station-details/');
-        this.plugs = response.data;
-      },
-      getPlugsFromStation(id) {
-        let ret = [];
-        for (let i = 0; i < this.plugs.length; i++) {
-          let plug = this.plugs[i];
-          if (plug.parentStation == id) {
-            ret.push(plug);
-          }
+                this.$emit("drawer", event);
+            },
+            async loadStations() {
+                let response = await axios.get('https://ipchannels.integreen-life.bz.it/emobility/rest/get-station-details/');
+                this.stations = response.data;
+            },
+            async loadPlugs() {
+                let response = await axios.get('https://ipchannels.integreen-life.bz.it/emobility/rest/plugs/get-station-details/');
+                this.plugs = response.data;
+            },
+            getPlugsFromStation(id) {
+                let ret = [];
+                for (let i = 0; i < this.plugs.length; i++) {
+                    let plug = this.plugs[i];
+                    if (plug.parentStation == id) {
+                        ret.push(plug);
+                    }
+                }
+                return ret;
+            }
         }
-        return ret;
-      }
     }
-  }
 </script>
 
 <style>
-  table {
-    width: 100%;
-  }
+    table {
+        width: 100%;
+    }
 
-  tr {
-    width: 100%;
-  }
+    tr {
+        width: 100%;
+    }
 
-  .zindexmax {
-    z-index: 1000;
-  }
+    .zindexmax {
+        z-index: 1000;
+    }
 </style>
