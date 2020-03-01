2b49bf705ebf4ca92857ee049fa5245e0353e42c
simonmx1 <simon@muscatello.bz.it>
2020-03-01 16:51:35 +0100

    schön

--- src/App.vue
+++ src/App.vue
@@ -51,6 +51,7 @@
                     <v-icon x-large @click.stop="closeDrawer()">mdi-chevron-double-right</v-icon>
                     <h3>{{this.station.name}}</h3>
                 </v-card-title>
+                <br>
                 <ul>
                     Ort: {{this.station.city}}
                     <br>
@@ -59,6 +60,7 @@
                     Access Type: {{this.station.accessType}}
                     <br>
                     Status: {{this.station.state}}
+                    <br>
                     <v-list v-for="plug in this.plugs" :key="plug.id">
                         <v-icon>mdi-battery-positive</v-icon>
                         {{plug.name}}
